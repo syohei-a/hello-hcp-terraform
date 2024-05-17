b6b0fc74dedac5243cb671c010f5b9fd61e1dd06
syohei-a <syohei_a@tokyo-gas.co.jp>
2024-05-17 07:14:18 +0000

    modified to t2.micro

--- main.tf
+++ main.tf
@@ -12,6 +12,6 @@ module "devinstance" {
   source  = "app.terraform.io/syohei-a/devinstance/aws"
   version = "0.0.2"
 
-  hello_tf_instance_type = "t2.large"
+  hello_tf_instance_type = "t2.micro"
   prefix                 = "syohei-a"
 }
